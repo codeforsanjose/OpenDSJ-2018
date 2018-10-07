d267c8601ae48cf103b204a3f8925102bd3cc523
Joseph <jrichardson@jmstudios.net>
2018-10-07 11:59:18 -0700

    remove reference

--- react-frontend/client/containers/App.js
+++ react-frontend/client/containers/App.js
@@ -9,7 +9,6 @@ class App extends Component {
         return (
             <div className='main'>
                 Hello World!
-                <LocalVoteMattersComponent />
             </div>
         );
     }
