f2a303fbb32312177797ff6e16fdf1ae5f390668
Joseph <jrichardson@jmstudios.net>
2018-10-07 14:16:36 -0700

    added elections function

--- react-frontend/server.js
+++ react-frontend/server.js
@@ -28,30 +28,11 @@ const getRepresentativeInfo = (address) => {
     })
 
 }
-
-getRepresentativeInfo('1184 normandy drive campbell ca 95008');
-
-/**
- * 
- * def __init__(self):
-        self.API_KEY = 'AIzaSyCWhwRupMs7IeE4IrGEgHtT0Nt-IGZnP9E'
-        self.endURL = '&key='+ self.API_KEY
-        self.baseRepURL = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
-        self.baseElectionsURL = 'https://www.googleapis.com/civicinfo/v2/elections?alt=json&prettyPrint=true'
-        self.url_handler = RequestHandler()
-        #self.baseElectionsURL = 'https://www.googleapis.com/civicinfo/v2/voterinfo?address='
-
-    def get_representative_info(self, address):
-        formatted_address = address.replace(' ', '+')
-        representative_url = '{}{}{}'.format(self.baseRepURL, formatted_address, self.endURL)
-        return self.url_handler.get_with_url(representative_url)
-
-    def get_all_elections_info(self):
-        elections_url = '{}{}'.format(self.baseElectionsURL, self.endURL)
-        return self.url_handler.get_with_url(elections_url)
-
- */
-
+const getElectionsInfo = () => {
+    const API_KEY = 'AIzaSyCWhwRupMs7IeE4IrGEgHtT0Nt-IGZnP9E';
+    const endURL = '&key='+ API_KEY;
+    const baseElectionsURL = `https://www.googleapis.com/civicinfo/v2/elections?alt=json&prettyPrint=true${endURL}`;
+}
 
 app.listen(8080, () => {
     console.log('Express server is up on port ' + 8080);
