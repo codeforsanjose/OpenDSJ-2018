cd0605c5e2bdd82746b2851b67e15907d3f647f3
Joseph <jrichardson@jmstudios.net>
2018-10-07 18:40:42 -0700

    cleanup

--- react-frontend/client/containers/App.js
+++ react-frontend/client/containers/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import '../styles/app.scss';
+
 import { mapStateToProps } from '../redux/selectors/App';
-import LocalOfficialsOfficesLookup from '../compositions/LocalOfficialsOfficesLookup/LocalOfficalsOfficesLookup'
+
+import '../styles/app.scss';
 
 class App extends Component {
     render() {
         return (
             <div className='main'>
                 Hello World!
-                <LocalOfficialsOfficesLookup />
             </div>
         );
     }
