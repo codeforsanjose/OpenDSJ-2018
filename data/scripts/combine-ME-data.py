78ea44beee5ad8afaa150f398a1af077e1eb7c77
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-10-18 09:44:42 -0700

    add script for concatenating ME campaign finance data xls files into one xls file

---
+++ data/scripts/combine-ME-data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# filenames
+file_names = [
+  "../11:6:2018-General-Election-SJC-candidate-campaign-data/SJC_D7_MayaEsparza/ME_460 Recipient Committee Campaign Statement 1:30:2018 1:1:2017 - 12:31:2017.xls", 
+  "../11:6:2018-General-Election-SJC-candidate-campaign-data/SJC_D7_MayaEsparza/ME_460 Recipient Committee Campaign Statement 4:26:2018 1:1:2018 - 4:21:2018.xls", 
+  "../11:6:2018-General-Election-SJC-candidate-campaign-data/SJC_D7_MayaEsparza/ME_460 Recipient Committee Campaign Statement 5:24:2018 4:22:2018 - 5:19:2018.xls", 
+  "../11:6:2018-General-Election-SJC-candidate-campaign-data/SJC_D7_MayaEsparza/ME_460 Recipient Committee Campaign Statement 6:4:2018 5:20:2018 - 6:3:2018.xls", 
+  "../11:6:2018-General-Election-SJC-candidate-campaign-data/SJC_D7_MayaEsparza/ME_460 Recipient Committee Campaign Statement 7:31:2018 6:4:2018 - 6:30:2018.xls", 
+  "../11:6:2018-General-Election-SJC-candidate-campaign-data/SJC_D7_MayaEsparza/ME_460 Recipient Committee Campaign Statement 9:27:2018 7:1:2018 - 9:22:2018.xls"
+]
+
+# read the files
+files = [pd.ExcelFile(name) for name in file_names]
+
+# turn the files into dataframes
+frames = [x.parse(x.sheet_names[0], header=None,index_col=None) for x in files]
+
+# delete the first row for all frames except the first
+frames[1:] = [df[1:] for df in frames[1:]]
+
+# concatenate the files
+combined = pd.concat(frames)
+
+# write it out
+combined.to_excel("combined-ME-460.xls", header=False, index=False)
