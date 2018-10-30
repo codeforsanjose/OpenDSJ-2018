4caf78469e28dd51a0ce171dd10bf4986265f5a5
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-10-30 12:59:52 -0700

    scripts to convert SCC BOE candidate A and E data into json

---
+++ data/scripts/convert-SCC-BOE-Kathleen-King-A-xls-to-json.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+#filename
+file_name = [
+  "../11:6:2018-General-Election-SCC-Board-of-Education-candidates/SCC-Board-of-Education-Area-2-candidates/KathleenKing-A-Contributions-10302018.xls"
+]
+
+# read the file
+excel_file = [pd.ExcelFile(name) for name in file_name]
+
+# turn the excel_file into dataframes
+frames = [x.parse(x.sheet_names[0], header=None,index_col=None) for x in excel_file]
+
+# convert to JSON
+[df.to_json("../json-11-6-2018-General-Election-SCC-Board-of-Education-candidates/Kathleen-King-A-Contributions-data.json",orient="table") for df in frames]
